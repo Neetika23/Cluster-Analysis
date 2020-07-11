d18fa076c114544e809613d3c5cffd15168d5a7d
Neetika23 <42495275+Neetika23@users.noreply.github.com>
2020-07-11 14:03:48 +0530

    Create Extract_dominant_colors.py

---
+++ Clustering in Real Life/Extract_dominant_colors.py
@@ -0,0 +1,54 @@
+# Import image class of matplotlib
+import matplotlib.image as img
+
+# Read batman image and print dimensions
+batman_image = img.imread('batman.jpg')
+print(batman_image.shape)
+
+# Store RGB values of all pixels in lists r, g and b
+for row in batman_image:
+    for temp_r, temp_g, temp_b in row:
+        r.append(temp_r)
+        g.append(temp_g)
+        b.append(temp_b)
+
+#The RGB values are stored in a data frame, batman_df. 
+The RGB values have been standardized used the whiten() 
+function, stored in columns, scaled_red, scaled_blue and scaled_green.
+
+distortions = []
+num_clusters = range(1, 7)
+
+# Create a list of distortions from the kmeans function
+for i in num_clusters:
+    cluster_centers, distortion = kmeans(batman_df[['scaled_red', 'scaled_blue', 'scaled_green']], i)
+    distortions.append(distortion)
+
+# Create a data frame with two lists, num_clusters and distortions
+elbow_plot = pd.DataFrame({'num_clusters': num_clusters, 'distortions': distortions})
+
+# Create a line plot of num_clusters and distortions
+sns.lineplot(x='num_clusters', y='distortions', data = elbow_plot)
+plt.xticks(num_clusters)
+plt.show()
+
+#Elbow plot constructed
+#Notice that there are three distinct colors present in the image, which is supported by the elbow plot.
+
+# Get standard deviations of each color
+r_std,g_std,b_std = batman_df[['red', 'green', 'blue']].std()
+
+for cluster_center in cluster_centers:
+    scaled_r, scaled_g, scaled_b = cluster_center
+    # Convert each standardized value to scaled value
+    colors.append((
+        scaled_r * r_std / 255,
+        scaled_g * g_std / 255,
+        scaled_b * b_std / 255
+    ))
+
+# Display colors of cluster centers
+plt.imshow([colors])
+plt.show()
+
+#Colors extracted
